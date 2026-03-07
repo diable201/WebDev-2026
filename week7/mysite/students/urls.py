90e044641047067ffdb45e6e6cb0d13e95e65c30
Sanzhar <diable201@protonmail.com>
2026-03-07 19:44:30 +0500

    added: week 7 examples

---
+++ week7/mysite/students/urls.py
@@ -0,0 +1,10 @@
+# 9. students/urls.py — создать файл
+from django.urls import path
+from . import views
+
+app_name = 'students'
+
+urlpatterns = [
+    path('', views.list_view, name='list'),
+    path('<int:pk>/', views.detail_view, name='detail'),
+]
---
+++ week7/mysite/students/views.py
@@ -0,0 +1,16 @@
+# 8. students/views.py — простые views
+from django.http import HttpResponse
+from .models import Student
+
+def list_view(request):
+    students = Student.objects.all()
+    html = "<h1>Students</h1><ul>"
+    for s in students:
+        html += f"<li>{s.name} — GPA:{s.gpa}</li>"
+    html += "</ul>"
+    return HttpResponse(html)
+
+def detail_view(request, pk):
+    student = Student.objects.get(pk=pk)
+    return HttpResponse(f"<h1>{student.name}</h1><p>GPA:{student.gpa}</p>")
+
